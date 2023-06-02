bd4f18bd015a5275fb40081c43accd3ccd4e44aa
Luis Escobar Salas <luis.escobar.s@usach.cl>
2023-06-01 23:09:18 -0400

    Quinto Commit: JS

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+Cambiar color de barra de navegación entre transparente y dark
+ var nav = document.querySelector("nav");
+ window.addEventListener("scroll", function () {
+   if (window.pageYOffset > 173) {
+     nav.classList.add("bg-dark", "shadow");
+   } else {
+     nav.classList.remove("bg-dark", "shadow");
+   }
+ });
+ 
+ Hacer smooth-scroll
+ $(function () {
+     $("a").click(function (event) {
+       if (this.hash !== "") {
+         event.preventDefault();
+  
+         var gato = this.hash;
+  
+         $("html, body").animate(
+           {
+             scrollTop: $(gato).offset().top - 50,
+           },
+           800,
+           function () {
+             window.location.hash = gato;
+           }
+         );
+       }
+     });
+   });
+
+   //Tooltip (footer)
+ var tooltipTriggerList = [].slice.call(
+     document.querySelectorAll('[data-bs-toggle="tooltip"]')
+   );
+   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+     return new bootstrap.Tooltip(tooltipTriggerEl);
+   });
